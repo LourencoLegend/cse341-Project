0e2587682e9765d2149d983d13c27bcc044b8c59
LourencoLegend <lourencolegend@yahoo.com>
2023-11-11 09:09:08 -0700

    Contacts uploaded

--- app.js
+++ app.js
@@ -1,10 +1,19 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
 const mongodb = require('./db/connect');
+
 const port = process.env.PORT || 8080;
+const app = express();
 
-app.use('/', require('./routes'))// http://localhost:8080
+app
+  .use(bodyParser.json())
+  .use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    next();
+  })
+  .use('/', require('./routes'));
 
+  
 mongodb.initDb((err, mongodb) => {
   if (err) {
     console.log(err);
